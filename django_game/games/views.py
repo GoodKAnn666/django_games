e5217b08653b59abf16b8ac94e8bf6b7ebd47c67
Mihail <mihail.strijanov@gmail.com>
2023-07-30 21:10:09 +0300

    created games pages

--- django_game/games/views.py
+++ django_game/games/views.py
@@ -7,7 +7,7 @@ from .models import Game
 class GamesView(ListView):
     model = Game
     queryset = Game.objects.filter(draft=False)
-    template_name = "games/games_list.html"
+    template_name = "games/game_list.html"
 
 
 class GameDetailView(DetailView):
