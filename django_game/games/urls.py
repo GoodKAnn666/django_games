b5c19a7b296a7c9a0728c25579ad6eb0ce299a37
Mihail <mihail.strijanov@gmail.com>
2023-07-31 03:45:19 +0300

    frames and reviews

--- django_game/games/urls.py
+++ django_game/games/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 urlpatterns = [
     path("", views.GamesView.as_view()),
-    path("<slug:slug>/", views.GameDetailView.as_view(), name="game_detail")
+    path("<slug:slug>/", views.GameDetailView.as_view(), name="game_detail"),
+    path("review/<int:pk>/", views.AddReview.as_view(), name="add_review"),
 ]
--- django_game/games/views.py
+++ django_game/games/views.py
@@ -1,9 +1,9 @@
 from django.views.generic import ListView, DetailView
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.base import View
 
 from .models import Game
-
+from .forms import ReviewForm
 
 class GamesView(ListView):
     model = Game
@@ -14,3 +14,13 @@ class GamesView(ListView):
 class GameDetailView(DetailView):
     model = Game
     slug_field = "url"
+
+class AddReview(View):
+    def post(self, request, pk):
+        form = ReviewForm(request.POST)
+        game = Game.objects.get(id=pk)
+        if form.is_valid():
+            form = form.save(commit=False)
+            form.game = game
+            form.save()
+        return redirect(game.get_absolute_url())
