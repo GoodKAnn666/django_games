b168a80668d23ad9d134672a64afb6256c159868
Mihail <mihail.strijanov@gmail.com>
2023-08-07 23:24:27 +0300

    updated authorization and registration

--- django_game/django_game/urls.py
+++ django_game/django_game/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.urls import path, include
 
@@ -23,11 +24,12 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("games.urls")),
     path('ckeditor/', include('ckeditor_uploader.urls')),
+    path('i18n/', include('django.conf.urls.i18n')),
 ]
 
-urlpatterns += [
-    path('accounts/', include('django.contrib.auth.urls')),
-]
+urlpatterns += i18n_patterns(
+    path('accounts/', include('allauth.urls')),
+)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
